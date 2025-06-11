e44387c870f44af52d0479a3342748c8e02ac5c5
hiepluu <luuhiep1804@gmail.com>
2025-06-11 18:35:31 +0700

    note for list image api

--- src/load-images/load-images.controller.ts
+++ src/load-images/load-images.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Get, Query } from '@nestjs/common';
 import { LoadImagesService } from './load-images.service';
-import { ApiOkResponse } from '@nestjs/swagger';
+import { ApiOkResponse, ApiQuery } from '@nestjs/swagger';
 import { ImageResultDTO } from './load-images.dto';
 
 @Controller('load-images')
@@ -12,6 +12,8 @@ export class LoadImagesController {
         description: "Get list images",
         type: ImageResultDTO
     })
+    @ApiQuery({ name: 'page', required: false, type: Number, example: 1 })
+    @ApiQuery({ name: 'limit', required: false, type: Number, example: 7 })
     imagesForLazyLoad(@Query('page') page = 1, @Query('limit') limit = 7) {
         return this.loadImageService.getImages(page,limit);
     }
