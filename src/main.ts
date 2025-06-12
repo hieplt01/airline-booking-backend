642ba844db68b9dee627cd1d4f1f2cf7322da164
hiepluu <luuhiep1804@gmail.com>
2025-06-12 10:25:04 +0700

    enable cors policy

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({ origin: true });
+
   const config = new DocumentBuilder()
     .setTitle('Airline Booking')
     .setDescription('The airline booking API description')
